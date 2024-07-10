7c4f3a72e1f74aea41aa82b49518226800a3f896
f13rc3 <ashish.rout406@gmail.com>
2024-07-10 22:07:13 +0530

    day 72

--- Video 69/script.js
+++ Video 69/script.js
@@ -1,9 +1,9 @@
 let a = 7
 
 function factorial(number){
-   let arr = Array.from(Array(number+1).keys())
-   let c = arr.slice(1,).reduce((a, b)=> a*b )
-   return c
+    arr = Array.from(Array(number+1).keys())
+    fact = arr.slice(1,).reduce((a,b)=> a*b)
+    return fact
 }
 
 
